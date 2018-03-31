c59bb3236087ffb25d5c013c6f249914599c1f6d
Lukas Steffen <lukas@stable.steffen-steffen.ch>
2018-03-31 21:51:44 +0200

    Add first view and template as well as urls.

--- blog/views.py
+++ blog/views.py
@@ -1,3 +1,8 @@
 from django.shortcuts import render
 
 # Create your views here.
+
+def post_list(request):
+    """ Render a list of all posts.
+    """
+    return render(request, 'blog/post_list.html', {})
